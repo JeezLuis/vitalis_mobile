b178b6a7dc283d9e746da8f8b25366403d7f1b32
Lluis Farré Castán <37417091+JeezLuis@users.noreply.github.com>
2023-07-21 12:35:01 +0200

    Welcome Page Implemented

---
+++ lib/utils.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
+
+//Generates HexColor from hex string
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+
+void alertError(String message, BuildContext context){
+  QuickAlert.show(
+    context: context,
+    type: QuickAlertType.error,
+    title: 'Oops...',
+    text: message,
+    confirmBtnColor: HexColor("868C9E"),
+    backgroundColor: HexColor("FBF1E1"),
+  );
+}
+
+void alertInfo(String message, BuildContext context){
+  QuickAlert.show(
+    context: context,
+    type: QuickAlertType.success,
+    text: message,
+    confirmBtnColor: HexColor("868C9E"),
+    backgroundColor: HexColor("FBF1E1"),
+  );
+}
